1e81364466e517b75be362db5784199c01c839d2
Chuang Hua <36153206+dryadd44651@users.noreply.github.com>
2020-06-08 21:02:38 -0500

    Create sortList-select-dummy.cpp

---
+++ C++/Data-Strctures/sortList-select-dummy.cpp
@@ -0,0 +1,50 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode* sortList(ListNode* head) {
+        ListNode *dummy = new ListNode();
+        dummy->next = head;
+        
+        ListNode *curr1 = head,*curr2 = head;
+        ListNode *prev1 = dummy,*prev2 = dummy;
+        while(curr1){
+            curr2 = curr1;
+            while(curr2){
+                if(curr2->val < curr1->val){
+                    ListNode *tmp = curr2->next;
+                    bool isHead = curr1==head;
+
+                    prev1->next = curr2;
+                    if(prev2!=curr1){
+                        prev2->next = curr1;
+                        curr2->next = curr1->next;
+                        curr1->next = tmp;
+                    }
+                    else{//consecutive node
+                        prev2->next = curr2->next;
+                        curr2->next = curr1;
+                    }
+                    tmp = curr1;
+                    curr1 = curr2;
+                    curr2 = tmp;
+                    
+                }
+                prev2 = curr2;
+                curr2 = curr2->next;
+            }
+            prev1 = curr1;
+            curr1 = curr1->next;
+        }
+    return dummy->next;
+    }
+};
