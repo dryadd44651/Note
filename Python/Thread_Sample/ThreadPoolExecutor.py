6268e60a22ec5a2c436bf317356d93a8b0f23394
Chuang Hua <36153206+dryadd44651@users.noreply.github.com>
2020-09-12 20:10:59 -0500

    Create ThreadPoolExecutor.py

---
+++ Python/Thread_Sample/ThreadPoolExecutor.py
@@ -0,0 +1,20 @@
+from concurrent.futures import ThreadPoolExecutor
+from time import sleep
+def task(message,n):
+    sleep(n/10)
+    print(n)
+    return message
+
+def main():
+    executor = ThreadPoolExecutor(3)
+    task3 = executor.submit(task, ("Completed"),3)
+    task2 = executor.submit(task, ("Completed"),2)
+    task1 = executor.submit(task, ("Completed"),1)
+    task0 = executor.submit(task, ("Completed"),0)
+    print(task1.done())
+    sleep(2/10)
+    print(task1.done())
+if __name__ == '__main__':
+    main()
+# task3 come first, but task1 print first
+# task0 print after task1, because of the number of thread
