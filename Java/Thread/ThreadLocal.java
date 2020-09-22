f984e22fc518eeaa68cf154f3ac597770403c652
Chuang Hua <36153206+dryadd44651@users.noreply.github.com>
2020-09-22 13:46:26 -0500

    Create ThreadLocal.java

---
+++ Java/Thread/ThreadLocal.java
@@ -0,0 +1,38 @@
+public class ThreadLocalDemo { 
+  
+public static void main(String[] args) 
+    { 
+  
+        ThreadLocal<Number> gfg_local = new ThreadLocal<Number>(); 
+  
+        ThreadLocal<String> gfg = new ThreadLocal<String>(); 
+        // setting the value 
+        gfg_local.set(100); 
+  
+        // returns the current thread's value 
+        System.out.println("value = " + gfg_local.get()); 
+  
+        // setting the value 
+        gfg_local.set(90); 
+  
+        // returns the current thread's value of 
+        System.out.println("value = " + gfg_local.get()); 
+  
+        // setting the value 
+        gfg_local.set(88.45); 
+  
+        // returns the current thread's value of 
+        System.out.println("value = " + gfg_local.get()); 
+  
+        // setting the value 
+        gfg.set("GeeksforGeeks"); 
+  
+        // returns the current thread's value of 
+        System.out.println("value = " + gfg.get()); 
+    } 
+} 
+
+value = 100
+value = 90
+value = 88.45
+value = GeeksforGeeks
