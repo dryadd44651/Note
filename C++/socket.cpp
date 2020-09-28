0c21a0fe3479c27d57e409b64a936a91aa67ec90
Chuang Hua <36153206+dryadd44651@users.noreply.github.com>
2020-09-28 16:10:34 -0500

    Create socket.cpp

---
+++ C++/socket.cpp
@@ -0,0 +1,105 @@
+//  C/C++ program to demonstrate Socket programming 
+#include <sys/socket.h> 
+#include <arpa/inet.h> 
+#include <unistd.h> 
+#include <iostream>
+#include <string>
+#include <thread>
+#include <netinet/in.h> 
+#define PORT 8080 
+
+void client() 
+{ 
+	int sock = 0, valread; 
+	struct sockaddr_in serv_addr; 
+	char *hello = "Hello from client"; 
+	char buffer[1024] = {0}; 
+	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
+	{ 
+		printf("\n Socket creation error \n"); 
+		return; 
+	} 
+
+	serv_addr.sin_family = AF_INET; 
+	serv_addr.sin_port = htons(PORT); 
+	
+	// Convert IPv4 and IPv6 addresses from text to binary form 
+	if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0) 
+	{ 
+		printf("\nInvalid address/ Address not supported \n"); 
+		return; 
+	} 
+
+	if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) 
+	{ 
+		printf("\nConnection Failed \n"); 
+		return; 
+	} 
+	send(sock , hello , strlen(hello) , 0 ); 
+	printf("Client: Hello message sent\n"); 
+	valread = read( sock , buffer, 1024); 
+	printf("Client Get:%s\n",buffer ); 
+} 
+// Server side C/C++ program to demonstrate Socket programming 
+
+void server() 
+{ 
+	int server_fd, new_socket, valread; 
+	struct sockaddr_in address; 
+	int opt = 1; 
+	int addrlen = sizeof(address); 
+	char buffer[1024] = {0}; 
+	char *hello = "Hello from server"; 
+	
+	// Creating socket file descriptor 
+	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
+	{ 
+		perror("socket failed"); 
+		exit(EXIT_FAILURE); 
+	} 
+	
+	// Forcefully attaching socket to the port 8080 
+	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, 
+												&opt, sizeof(opt))) 
+	{ 
+		perror("setsockopt"); 
+		exit(EXIT_FAILURE); 
+	} 
+	address.sin_family = AF_INET; 
+	address.sin_addr.s_addr = INADDR_ANY; 
+	address.sin_port = htons( PORT ); 
+	
+	// Forcefully attaching socket to the port 8080 
+	if (bind(server_fd, (struct sockaddr *)&address, 
+								sizeof(address))<0) 
+	{ 
+		perror("bind failed"); 
+		exit(EXIT_FAILURE); 
+	} 
+	if (listen(server_fd, 3) < 0) 
+	{ 
+		perror("listen"); 
+		exit(EXIT_FAILURE); 
+	} 
+	if ((new_socket = accept(server_fd, (struct sockaddr *)&address, 
+					(socklen_t*)&addrlen))<0) 
+	{ 
+		perror("accept"); 
+		exit(EXIT_FAILURE); 
+	} 
+	valread = read( new_socket , buffer, 1024); 
+	printf("Server Get: %s\n",buffer ); 
+	send(new_socket , hello , strlen(hello) , 0 ); 
+	printf("Server: Hello message sent\n"); 
+} 
+ 
+
+int main()
+{
+    std::thread a(server), b(client);
+ 
+ 
+    a.join();
+    b.join();
+
+}
