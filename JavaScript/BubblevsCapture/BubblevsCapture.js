da59b5aa610a4a808792025984c8fa9f68a0361f
Chuang Hua <36153206+dryadd44651@users.noreply.github.com>
2020-10-01 22:50:18 -0500

    Create BubblevsCapture.js

---
+++ JavaScript/BubblevsCapture/BubblevsCapture.js
@@ -0,0 +1,70 @@
+// BUBBLE
+// document.querySelector('.box-1').addEventListener(
+//     'click',
+//     e => {
+//         setTimeout(() => {
+//             document.querySelector('.box-1 > p').textContent = 'Clicked';
+//             console.log('Box-1 is clicked');
+//         }, 1000);
+//     },
+//     
+// );
+
+// document.querySelector('.box-2').addEventListener(
+//     'click',
+//     e => {
+//         setTimeout(() => {
+//             document.querySelector('.box-2 > p').textContent = 'Clicked';
+//             console.log('Box-2 is clicked');
+//         }, 1000);
+//     },
+//     
+// );
+
+// document.querySelector('.box-3').addEventListener(
+//     'click',
+//     e => {
+//         setTimeout(() => {
+//             document.querySelector('.box-3 > p').textContent = 'Clicked';
+//             console.log('Box-3 is clicked');
+//         }, 1000);
+//     },
+//     
+// );
+// CAPTURE 
+document.querySelector('.box-1').addEventListener(
+    'click',
+    e => {
+        setTimeout(() => {
+            document.querySelector('.box-1 > p').textContent = 'Clicked';
+            console.log('Box-1 is clicked');
+        }, 1000);
+    },
+    true
+);
+
+document.querySelector('.box-2').addEventListener(
+    'click',
+    e => {
+        setTimeout(() => {
+            document.querySelector('.box-2 > p').textContent = 'Clicked';
+            console.log('Box-2 is clicked');
+        }, 1000);
+    },
+    true
+);
+
+document.querySelector('.box-3').addEventListener(
+    'click',
+    e => {
+        setTimeout(() => {
+            document.querySelector('.box-3 > p').textContent = 'Clicked';
+            console.log('Box-3 is clicked');
+        }, 1000);
+    },
+    true
+);
+
+//"Box-1 is clicked"
+//"Box-2 is clicked"
+//"Box-3 is clicked"
