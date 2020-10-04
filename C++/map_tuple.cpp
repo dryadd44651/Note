e36bd9d376f61705ed09841c67f8938c424e7ba8
Chuang Hua <36153206+dryadd44651@users.noreply.github.com>
2020-10-04 18:34:37 -0500

    Update map_tuple.cpp

--- C++/map_tuple.cpp
+++ C++/map_tuple.cpp
@@ -1,10 +1,13 @@
 int main() {
-    auto r1 = make_tuple(3.8, 'A', "Lisa Simpson");
-    auto r2 = make_tuple(2.9, 'C', "Milhouse Van Houten");
-    auto r3 = make_tuple(1.7, 'D', "Ralph Wiggum");
+    // auto r1 = make_tuple(3.8, 'A', "Lisa Simpson");
+    // auto r2 = make_tuple(2.9, 'C', "Milhouse Van Houten");
+    tuple<double,char,string> r3 = {1.7, 'D', "Ralph Wiggum"};
     map<string,tuple<double,char,string>> tmap;
-    tmap["r1"] = r1;
-    tmap["r2"] = r2;
+    // tmap["r1"] = r1;
+    // tmap["r2"] = r2;
+    // tmap["r3"] = r3;
+    tmap["r1"] = {3.8, 'A', "Lisa Simpson"};
+    tmap["r2"] = {2.9, 'C', "Milhouse Van Houten"};
     tmap["r3"] = r3;
     for(auto t:tmap){
         cout<<get<0>(t)<<" ";
