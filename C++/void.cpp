d88e945e727fa50035d25d502de76b23733c7b54
Chuang Hua <36153206+dryadd44651@users.noreply.github.com>
2020-10-04 20:33:48 -0500

    Update void.cpp

--- C++/void.cpp
+++ C++/void.cpp
@@ -1,9 +1,13 @@
 int main() {
     int a = 10; 
     void *ptr = &a; 
-    //printf("%d", *ptr); 
     cout<<ptr<<" "<<&a<<endl;
-    //cout<<(int)*ptr; void cant cast to int
+    cout<<*(int*)ptr<<endl;
+    
+    int b[10] = {1, 2, 3, 4, 5, 6, 7}; 
+    void *ptr1 = b; 
+    ptr1 = (int*)ptr1+1;     
+    cout<<*(int*)ptr1<<endl;
     return 0; 
         
 }
