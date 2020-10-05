9a224cc9c993f6523b3e7f70b308211f0a6bb1fa
Chuang Hua <36153206+dryadd44651@users.noreply.github.com>
2020-10-04 21:32:47 -0500

    Create classHash.java

---
+++ Java/classHash.java
@@ -0,0 +1,24 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> arrlist = new ArrayList<Integer>(); 
+        var hm = new HashMap< ArrayList<Integer>,Integer>(); 
+        for (int i=1; i<=9; i++) {
+            arrlist.add(i);
+            hm.put((ArrayList)arrlist.clone(),Integer.valueOf(i));
+        }
+        
+        
+        
+        for (var entry : hm.entrySet())
+            System.out.println(entry);
+        ArrayList<Integer> a = new ArrayList<Integer>(
+            List.of(1, 2, 3));
+        //get num (hm store the (ArrayList)arrlist.clone() hash value)
+        System.out.println(hm.get(hm.get(a)));
+        
+        
+    }
+}
+
