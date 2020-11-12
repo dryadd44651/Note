6ab65f75f6b5e19e29de891b56c79019ccf6c488
Chuang Hua <36153206+dryadd44651@users.noreply.github.com>
2020-11-12 11:45:06 -0600

    Create async.js

---
+++ JavaScript/async.js
@@ -0,0 +1,49 @@
+//javascript is synchronous language
+(async function main() {
+const sleep = m => new Promise(r => setTimeout(r, m))
+    //async example
+    async function abc()
+    {
+        //setTimeout(() => { console.log("World!"); }, 5);
+        console.time("Slept for")
+        await sleep(2)//sleep for 2ms
+        console.timeEnd("Slept for")
+        console.log('abc');
+    }
+    async function xyz()
+    {
+       await abc()
+       console.log('xyz');
+    }
+    await console.log('async');
+    await xyz();//async order: slept(2)->abc->xyz
+    
+    await console.log('=======================================');
+    await console.log('sync');
+    async function abc1()
+    {
+        setTimeout(() => { console.log("World!"); }, 5);
+        console.time("Slept for")
+        sleep(2)//no sleep
+        console.timeEnd("Slept for")
+        await sleep(2)
+        console.log('abc');
+    }
+    function xyz1()
+    {
+       abc1()
+       console.log('xyz');
+    }
+    await xyz1();//sync order: slept->xyz->abc(2ms)->World!(5ms)
+}());
+
+// async
+// Slept for: 3.082ms
+// abc
+// xyz
+// =======================================
+// sync
+// Slept for: 0.033ms
+// xyz
+// abc
+// World!
