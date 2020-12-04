3f9cdc6e388c431093b74b8bca7da0638f0a15ed
Chuang Hua <36153206+dryadd44651@users.noreply.github.com>
2020-12-03 18:52:42 -0600

    Update 3.index.sql

--- ICC/SQL/3.index.sql
+++ ICC/SQL/3.index.sql
@@ -11,62 +11,45 @@ select * from table(dbms_xplan.display);
 
 
 -- PL/SQL (function / stored procedure / trigger)
--- implicit cursor
--- explicit cursor
+--cursor is a pointer to get data
+-- implicit cursor (can't be changed like const)
+-- explicit cursor (can be changed)
 
--- --create table
--- create table stu(
---     name varchar(50)
--- )
--- --create trigger
--- create or replace trigger stu_trigger
--- before update of name on stu
--- for each row
--- begin
---     dbms_output.put_line('old name: '||:old.name);
---     dbms_output.put_line('new name: '||:new.name);
--- end;
--- --insert data
--- insert into stu values('Alic');
--- --update(trigger will be called)
--- update stu set name = 'Tom'
--- --check result
--- select * from stu
+--create the PLSQL function
+create or replace procedure findEmp1(emp_id in number) as
+    cursor emp_cursor is select first_name,last_name from hr.employees where employee_id >= emp_id;
+begin
+    for emp in emp_cursor loop
+        dbms_output.put_line(emp.first_name || ' ' || emp.last_name);
+    end loop;
+end;
 
--- -- create or replace function findEmpFunction return number as
--- -- begin
--- --     return 5;
--- -- end;
+begin
+    findEmp1(100);
+end;
+
+create or replace procedure findEmp2(emp_id in number) as
+    e_first_name hr.employees.first_name%type;
+    e_last_name hr.employees.last_name%type;
+    cursor emp_cursor is select first_name,last_name from hr.employees where employee_id >= emp_id;
+begin
+    open emp_cursor;
+    fetch emp_cursor into e_first_name,e_last_name;
+    dbms_output.put_line(e_first_name || ' ' || e_last_name);
+    while emp_cursor%found loop
+        fetch emp_cursor into e_first_name,e_last_name;
+        dbms_output.put_line(e_first_name || ' ' || e_last_name);
+    end loop;
+    close emp_cursor;
+    
+end;
 
 
--- -- begin
--- --     dbms_output.put_line(findEmpFunction());
--- -- end;
 
--- select first_name, findEmpFunction()
--- from hr.employees
+begin
+    findEmp2(100);
+end;
 
--- create or replace procedure findEmp(emp_id in number) as
---     e_first_name hr.employees.first_name%type;
---     cursor emp_cursor is select first_name from hr.employees where employee_id >= emp_id;
--- begin
---     --select first_name into e_first_name from hr.employees where employee_id >= emp_id;
---     -- for emp in emp_cursor loop
---     --     dbms_output.put_line(emp.first_name || ' ' || emp.last_name);
---     -- end loop;
---     open emp_cursor;
---     fetch emp_cursor into e_first_name;
---     dbms_output.put_line(e_first_name);
---     while emp_cursor%found loop
---         fetch emp_cursor into e_first_name;
---         dbms_output.put_line(e_first_name);
---     end loop;
---     close emp_cursor;
--- end;
-
--- begin
---     findEmp(100);
--- end;
 
 
 -- View
