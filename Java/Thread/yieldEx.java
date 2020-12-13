9ccbd869fd405d10d37234a456ac1d5459abb2b3
Chuang Hua <36153206+dryadd44651@users.noreply.github.com>
2020-12-13 15:37:26 -0600

    Create yieldEx.java

---
+++ Java/Thread/yieldEx.java
@@ -0,0 +1,68 @@
+// Java Program to demonstrate usuage of BlockingQueue 
+
+import java.util.concurrent.*; 
+import java.util.*;
+
+class yieldEx extends Thread{
+    
+    int num;
+    yieldEx(int num){
+        this.num = num;
+    }
+    @Override
+    public void run(){
+        try{
+            for(int i =0;i<40;i++){
+                System.out.println("("+this.num+" : "+i+")");   
+			    if (i % 10 == 0) {
+			        //force to yield the CPU time
+                    Thread.yield();
+			    }
+            }    
+        }catch(Exception e){
+            System.err.print("error");   
+        }
+
+    }
+    
+}
+
+
+public class Test { 
+    
+	public static void main(String[] args) throws InterruptedException 
+	{ 
+        yieldEx p1 = new yieldEx(1);
+        yieldEx p2 = new yieldEx(2);
+        // time slot will be very short
+        // p1.setPriority(Thread.MIN_PRIORITY);
+        // p2.setPriority(Thread.MIN_PRIORITY);
+        //time slot will be very long
+        p1.setPriority(Thread.MAX_PRIORITY);
+        p2.setPriority(Thread.MAX_PRIORITY);
+        p1.start();
+        p2.start();
+        
+	} 
+}
+// Thread must change when mod 10 = 0
+// (2 : 9)
+// (2 : 10)
+// (1 : 1)
+// (1 : 2)
+// .
+// .
+// .
+// (1 : 18)
+// (1 : 19)
+// (1 : 20)
+// (2 : 21)
+// (2 : 22)
+// .
+// .
+// .
+// (1 : 28)
+// (1 : 29)
+// (1 : 30)
+// (2 : 31)
+// (2 : 32)
