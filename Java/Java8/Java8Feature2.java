3a3762e86325c219ede30d22a5883b57900fa7ef
Chuang Hua <36153206+dryadd44651@users.noreply.github.com>
2020-12-14 22:11:49 -0600

    Update Java8Feature2.java

--- Java/Java8/Java8Feature2.java
+++ Java/Java8/Java8Feature2.java
@@ -10,14 +10,16 @@ class Demo
         List<Integer> number = Arrays.asList(6,7,1,2,0,2,3,4,5); 
         List<String> names = Arrays.asList("Steven","Seven","Collection","Stream"); 
 
-
-        // map filter sort
+        
+        // Intermediate Operations: map filter sort
         // map
         // use pre defined functional interface
         Function<Integer,Integer> mapper = (x) -> x*x;
         //number = number.stream().map(x -> x*x).collect(Collectors.toList()); 
         number = number.stream().map(mapper).collect(Collectors.toList()); 
         System.out.println(number);
+       
+        
         // filter
         names = names.stream().filter(s->s.startsWith("S")).collect(Collectors.toList()); 
         System.out.println(names);
@@ -30,12 +32,14 @@ class Demo
         System.out.println(names);
         number = number.stream().sorted().collect(Collectors.toList()); 
         System.out.println(number);
-
+        
+        //Terminal Operations: collect foreach reduce
         //foreach
         Consumer<Integer> cm=i->System.out.print(i+"-");
         //number.stream().map(x->x*x).forEach(y->System.out.print(y+"-"));
         number.stream().map(x->x*x).forEach(cm); 
         System.out.println();
+        
         // reduce
         // Optional: the class may or may not contain a non-null value( get() to get data)
         // Optional/Optional<Integer>/Object/var
