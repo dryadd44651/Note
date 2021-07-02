c3fb8e8ac4eb95398a9787998657a3bf5860b353
Chuang Hua <36153206+dryadd44651@users.noreply.github.com>
2021-07-01 22:22:41 -0500

    Delete Java/JPA directory

--- Java/JPA/demo/src/main/java/com/example/jpa/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.jpa;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-// <object, ID(key)>
-public interface UserRepository extends JpaRepository<User, Long>{
-
-}
--- Java/JPA/license/src/main/java/com/foxconn/license/entity/LicenseRepository.java
+++
@@ -1,7 +0,0 @@
-package com.foxconn.license.entity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LicenseRepository extends JpaRepository<License, String>{
-}
